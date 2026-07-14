5f8a11ad97d0716ac1e05a2af2f0f9efbcd482fb
Yogendar <yogendersinghrt48@gmail.com>
2026-07-14 10:40:55 +0530

    add subscription model

---
+++ Backend/src/Models/subcriptionModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+
+
+const SubscriptionSchema= new  mongoose.Schema({
+    subscriber:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        
+    },
+    channel:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+},{timestamps:true})
+
+
+const Subscription= mongoose.model("Subscription",SubscriptionSchema)
+
+export default Subscription;
